39c7a1866311cd7c1c6066fc5fac7c17c170859b
coder-frank <frankemmanuel249@gmail.com>
2023-06-15 00:55:18 -0700

    5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        char message[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s\n", message);
+	return (0);
+}
