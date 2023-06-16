03a127e3f75b318cc68fab58569015a21c4d324f
Ebuka Mbanusi <90049318+coder-frank@users.noreply.github.com>
2023-06-16 14:01:48 +0100

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - Entry point of program
+ * main - Entry point of the program
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char letter;
-	
+
 	for (letter = 'a'; letter <= 'z'; ++letter)
 		putchar(letter);
-	
+
 	for (letter = 'A'; letter <= 'Z'; ++letter)
 		putchar(letter);
 
-    	putchar('\n');
+	putchar('\n');
 	return (0);
 }
