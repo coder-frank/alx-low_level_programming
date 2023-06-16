7fa14b70c90740efdce6063878974bb07f1e73bf
Ebuka Mbanusi <90049318+coder-frank@users.noreply.github.com>
2023-06-16 10:29:01 +0100

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,15 +13,17 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0)
+	
+	if (n > 5)
 	{
-		puts("is positive");
+		message = "and is greater than 5";
 	} else if (n == 0)
 	{
-		puts("is zero");
-	} else
+		message = "and is 0";
+	} else if (n < 6 && n != 0)
 	{
-		puts("is negative");
+		message = "and is less than 6 and not 0";
 	}
+	printf("Last digit of %d is %s\n", n, message);
 	return (0);
 }
