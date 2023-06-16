9bbfe732ccf012b6e9f157953a3f34af0bff18b1
Ebuka Mbanusi <90049318+coder-frank@users.noreply.github.com>
2023-06-16 14:11:23 +0100

    Create 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  int i;
+  
+  while(i = 0; i < 10; i++)
+  {
+    printf("%d", i);
+  }
+  printf("\n");
+  return(0)
+}
