ebb6aaddab8695f831ca477c020fa8271bbe306b
Ebuka Mbanusi <90049318+coder-frank@users.noreply.github.com>
2023-06-16 14:06:21 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,35 +1,24 @@
 #include <stdio.h> 
 
-  
-
  /** 
-
   * main - Entry point of program 
-
   * 
-
   * Return: Always 0 (Success) 
-
   */ 
-
- int main(void) 
-
- { 
-
-         for (letter = 'a'; letter <= 'z'; ++letter) { 
-                 if(letter == 'q' || letter == 'e')
-                 {
-                         continue;
-                 } else 
-                 {
-                         putchar(letter); 
-                 }
-
-     } 
-     putchar('\n'); 
-
-  
-
-         return (0); 
-
+ int main(void)
+ {
+   char letter;
+   
+  for (letter = 'a'; letter <= 'z'; ++letter)
+  {
+    if(letter == 'q' || letter == 'e')
+    {
+      continue;
+    } else
+    {
+      putchar(letter);
+    }
+  }
+   putchar('\n'); 
+   return (0); 
  }
