24910d0d41eea39ac4a916e81069745e28c8cb67
Ebuka Mbanusi <90049318+coder-frank@users.noreply.github.com>
2023-06-16 13:55:24 +0100

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,13 +7,14 @@
  */
 int main(void)
 {
-	for (letter = 'a'; letter <= 'z'; ++letter) {
-        putchar(letter);
-    }
-   for (letter = 'A'; letter <= 'Z'; ++letter) {
-        putchar(letter);
-    }
-    putchar('\n');
+	char letter;
+	
+	for (letter = 'a'; letter <= 'z'; ++letter) 
+        	putchar(letter);
+	
+	for (letter = 'A'; letter <= 'Z'; ++letter) 
+        	putchar(letter);
+    	putchar('\n');
 	
 	return (0);
 }
