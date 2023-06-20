29b021118bb2ee3b69736e3faa6abef11b020a0d
coder-frank <frankemmanuel249@gmail.com>
2023-06-20 01:35:17 -0700

    5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ *
+ * Return: Always 0 (Success)
+ */
+int print_sign(int n)
+{
+	int r;
+
+	if (n > 0)
+	{
+		putchar('+');
+		r = 1;
+	} else if (n == 0)
+	{
+		putchar('0');
+		r = 0;
+	} else
+	{
+		putchar('-');
+		r = -1;
+	}
+	return (r);
+}
+
+int main(void)
+{
+	r = print_sign(98);
+	putchar(',');
+	putchar(' ');
+	putchar(r + '0');
+	putchar('\n');
+	r = print_sign(0);
+	putchar(',');
+	putchar(' ');
+	putchar(r + '0');
+	putchar('\n');
+	r = print_sign(0xff);
+	putchar(',');
+	putchar(' ');
+	putchar(r + '0');
+	putchar('\n');
+	r = print_sign(-1);
+	putchar(',');
+	putchar(' ');
+	putchar(r + '0');
+	putchar('\n');
+	return (0);
+}
