48cf34c90769c1905b0e8bbf230275b724e521f2
Ebuka Mbanusi <90049318+coder-frank@users.noreply.github.com>
2023-06-20 13:50:13 +0100

    Update main.c

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -9,14 +9,3 @@ int _putchar(char c)
     // Return the character as an integer
     return c;
 }
-
-void print_alphabet(void)
-{
-	char letter;
-
-	for (letter = 'a'; letter <= 'z'; ++letter)
-	{
-		__putchar(letter);
-	}
-	_putchar('\n');
-}
