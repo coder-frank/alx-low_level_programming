f0559e43a1ed1b4119f3988271860fb7d80d48dd
Ebuka Mbanusi <90049318+coder-frank@users.noreply.github.com>
2023-06-20 13:29:21 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,15 +6,6 @@
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet(void)
-{
-	char letter;
-
-	for (letter = 'a'; letter <= 'z'; ++letter)
-	{
-		putchar(letter);
-	}
-}
 
 int main(void)
 {
