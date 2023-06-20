0a4eb7ebe2b4e520f2b7f414193b28ecc63f0429
Ebuka Mbanusi <90049318+coder-frank@users.noreply.github.com>
2023-06-20 19:05:50 +0100

    Create 100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * times_table - prints the times table from 0 - a specified number
+ * @n: the number to be specified
+ * Return: Always 0 (Success)
+ */
+void times_table(int n)
+{
+int r, c, d;
+
+	for (r = 0; r <= n; r++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+	for (c = 1; c <= n; c++)
+	{
+		d = (r * c);
+	if ((d / 10) > 0)
+	{
+		_putchar((d / 10) + '0');
+	}
+	else
+	{
+		_putchar(' ');
+	}
+		putchar((d % 10) + '0');
+
+	if (c < 9)
+	{
+		_putchar(',');
+	     _putchar(' ');
+	}
+	}
+	putchar('\n');
+	}
+
+}
