4944dcf24bfee6071ce9c387870b5fd88d8871a7
Ebuka Mbanusi <90049318+coder-frank@users.noreply.github.com>
2023-06-20 15:17:10 +0100

    Update program.c

--- 0x02-functions_nested_loops/program.c
+++ 0x02-functions_nested_loops/program.c
@@ -2,11 +2,10 @@
 
 int main(void)
 {
-	int r;
 
-	r = print_sign(98);
-	r = print_sign(0);
-	r = print_sign(0xff);
-	r = print_sign(-1);
+	print_sign(98);
+	print_sign(0);
+	print_sign(0xff);
+	print_sign(-1);
 	return (0);
 }
