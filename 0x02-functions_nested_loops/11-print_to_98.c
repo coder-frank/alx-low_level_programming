abbe5f7b967b052834308cad7797ad0b808d4d82
Ebuka Mbanusi <90049318+coder-frank@users.noreply.github.com>
2023-06-20 17:21:01 +0100

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,13 +6,22 @@
  * @n : number to start from
  * Return: the number to 98
  */
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints from a number to 98
+ * @n: number to start from
+ * Return: void
+ */
 void print_to_98(int n)
 {
 	while (n != 98)
 	{
-		_putchar(n);
-		_putchar(',');
-		_putchar(' ');
-		n++;
+		printf("%d, ", n);
+		if (n > 98)
+			n--;
+		else
+			n++;
 	}
+	printf("98\n");
 }
