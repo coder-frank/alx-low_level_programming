8bbe0caced9fcbe3b0627772198ddb69f5aa931b
Ebuka Mbanusi <90049318+coder-frank@users.noreply.github.com>
2023-06-20 15:24:49 +0100

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,17 +8,22 @@
  * Return: 1 if the number greater than 0,
  * 0 if its equal to 0, otherwise -1
  */
-void print_sign(int n)
+int print_sign(int n)
 {
-
 	if (n > 0)
 	{
-		printf("+1");
-	} else if (n == 0)
-	{
-		printf("00");
-	} else
-	{
-		printf("-1");
+		_putchar(43);
+		return (1);
 	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+	_putchar('\n');
 }
