570fe823d12e4e374922e06fb8173cf48c6aed52
Ebuka Mbanusi <90049318+coder-frank@users.noreply.github.com>
2023-06-20 15:03:44 +0100

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,15 +14,15 @@ int print_sign(int n)
 
 	if (n > 0)
 	{
-		putchar('+');
+		_putchar('+');
 		r = 1;
 	} else if (n == 0)
 	{
-		putchar('0');
+		_putchar('0');
 		r = 0;
 	} else
 	{
-		putchar('-');
+		_putchar('-');
 		r = -1;
 	}
 	return (r);
