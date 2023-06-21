509477674ad7c33135ae600115603f75702b1261
Ebuka Mbanusi <90049318+coder-frank@users.noreply.github.com>
2023-06-21 19:25:18 +0100

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,12 +8,9 @@
 void fib(void)
 {
 	int i;
-	mpz_t f, s, r;
-	mpz_init(f);
-	mpz_init(s);
-	mpz_init(r);
-	mpz_set_ui(f, 1);
-	mpz_set_ui(s, 2);
+	int f, s, r;
+	f = 1;
+	s = 2;
 
 	for (i = 1; i <= 96; i++)
 	{
@@ -24,22 +21,19 @@ void fib(void)
 
 		if (i == 0)
 		{
-			gmp_printf("%Zd, %Zd, ", f, s);
+			printf("%d, %d, ", f, s);
 		}
 		if (i == 96)
 		{
 			mpz_add(r, f, s);
-			gmp_printf("%Zd\n", r);
+			printf("%d\n", r);
 			break;
 		}
-		mpz_add(r, f, s);
-		gmp_printf("%Zd,", r);
-		mpz_set(f, s);
-		mpz_set(s, r);
+		r = f + s;
+		printf("%d,", r);
+		f = s;
+		s = r;
 	}
-	mpz_clear(f);
-	mpz_clear(s);
-	mpz_clear(r);
 }
 /**
 * main - Executes the fib
