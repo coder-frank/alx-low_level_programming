2e3d9cad3eb5e52ec2726ea1fbb75627db6dad0f
Ebuka Mbanusi <90049318+coder-frank@users.noreply.github.com>
2023-06-21 17:50:22 +0100

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -12,7 +12,7 @@ void fib(void)
 	unsigned long s = 2;
 	unsigned long r;
 
-	for (i = 0; i <= 96; i++)
+	for (i = 0; i <= 93; i++)
 	{
 		if (i != 0)
 		{
@@ -23,7 +23,7 @@ void fib(void)
 		{
 			printf("%lu, %lu, ", f, s);
 		}
-		if (i == 96)
+		if (i == 93)
 		{
 			r = f + s;
 			printf("%lu\n", r);
