8b2930e16cdc952a088c4b721d5872abd642e9a2
Ebuka Mbanusi <90049318+coder-frank@users.noreply.github.com>
2023-06-21 17:36:50 +0100

    Create 103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+* fib - Prints fibonacci numbers of 50
+* main - Executes all code
+* Return: Always 0.
+*/
+void fib(void)
+{
+	int i;
+	unsigned long f = 1;
+	unsigned long s = 2;
+	unsigned long r;
+	unsigned long sum;
+
+	for (i = 1; i <= 4000000; i++)
+	{
+		if (r % 2 == 0)
+		{
+			sum += r;
+		}
+		r = f + s;
+	}
+	printf("%lu\n", sum);
+}
+/**
+* main - Executes the fib
+* Return: Always 0
+*/
+int main(void)
+{
+	fib();
+	return (0);
+}
