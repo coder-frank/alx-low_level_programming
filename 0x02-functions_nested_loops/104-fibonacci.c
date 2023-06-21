de04ebd075c2240f31b1bc6015e2b261a505bb4a
Ebuka Mbanusi <90049318+coder-frank@users.noreply.github.com>
2023-06-21 17:47:44 +0100

    Create 104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/**
+* fib - Prints fibonacci numbers of 50
+* main - Executes all code
+* Return: Always 0.
+*/
+void fib(void)
+{
+	int i;
+	unsigned long f = 1;
+	unsigned long s = 2;
+	unsigned long r;
+
+	for (i = 0; i <= 96; i++)
+	{
+		if (i != 0)
+		{
+			printf(" ");
+		}
+
+		if (i == 0)
+		{
+			printf("%lu, %lu, ", f, s);
+		}
+		if (i == 47)
+		{
+			r = f + s;
+			printf("%lu\n", r);
+			break;
+		}
+		r = f + s;
+		printf("%lu,", r);
+		f = s;
+		s = r;
+	}
+}
+/**
+* main - Executes the fib
+* Return: Always 0
+*/
+int main(void)
+{
+	fib();
+	return (0);
+}
