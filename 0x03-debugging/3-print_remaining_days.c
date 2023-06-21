928d2fde42dc19e0c8e90774a3da534e644750db
Ebuka Mbanusi <90049318+coder-frank@users.noreply.github.com>
2023-06-21 20:44:11 +0100

    Update 3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
 #include "main.h"
 /**
+  * check_year - checks if a number is a leap year or not
+  * @year: year to be checked
+  * Return: 1 if its a leap year, otherwise 0
+  */
+int check_year(int year)
+{
+	int is_leap_year;
+
+	if (year % 4 == 0)
+	{
+		if (year % 100 == 0)
+		{
+			if (year % 400 == 0)
+			{
+				is_leap_year = 1;
+			} else
+			{
+				is_leap_year = 0;
+			}
+		} else
+		{
+			is_leap_year = 1;
+		}
+	} else
+	{
+		is_leap_year = 0;
+	}
+	return (is_leap_year);
+}
+/**
  * print_remaining_days - takes a date and prints how many days are
  * left in the year, taking leap years into account
  * @month: month in number format
