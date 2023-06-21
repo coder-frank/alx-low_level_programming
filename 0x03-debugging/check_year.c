b6a43c74ecd9275b015fe14d2cc0f3fb53dd7e21
Ebuka Mbanusi <90049318+coder-frank@users.noreply.github.com>
2023-06-21 20:37:56 +0100

    Create check_year.c

---
+++ 0x03-debugging/check_year.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  * check_year - checks if a number is a leap year or not
+  * @year: year to be checked
+  * Return: 1 if its a leap year, otherwise 0
+  */
+int check_year(int year)
+{
+	int is_leap_year;
+
+	if (year % 4 == 0)
+	{
+		if (year % 100 == 0)
+		{
+			if (year % 400 == 0)
+			{
+				is_leap_year = 1;
+			} else
+			{
+				is_leap_year = 0;
+			}
+		} else
+		{
+			is_leap_year = 1;
+		}
+	} else
+	{
+		is_leap_year = 0;
+	}
+	return (is_leap_year);
+}
