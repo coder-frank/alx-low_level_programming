26989e59cd152f198dcad2407acca4bb836425b2
Ebuka Mbanusi <90049318+coder-frank@users.noreply.github.com>
2023-06-21 21:46:13 +0100

    Update 3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,27 +1,64 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  * check_year - checks if a number is a leap year or not
+  * @year: year to be checked
+  * Return: 1 if its a leap year, otherwise 0
+  */
+int check_year(int year)
+{
+	int is_leap_year;
+
+	if (year % 4 == 0)
+	{
+		if (year % 100 == 0)
+		{
+			if (year % 400 == 0)
+			{
+				is_leap_year = 1;
+			} else
+			{
+				is_leap_year = 0;
+			}
+		} else
+		{
+			is_leap_year = 1;
+		}
+	} else
+	{
+		is_leap_year = 0;
+	}
+	return (is_leap_year);
+}
+/**
+ * print_remaining_days - takes a date and prints how many days are
+ * left in the year, taking leap years into account
+ * @month: month in number format
+ * @day: day of year
+ * @year: year
+ */
 void print_remaining_days(int month, int day, int year)
 {
-	int days_in_month[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	int total_days = 365;
-	int remaining_days;
+	int is_leap_year = check_year(year);
 
-	if (is_leap_year(year))
+	if (is_leap_year == 1)
 	{
-		days_in_month[2] = 29;
-		total_days = 366;
-	}
-
-	if (month < 1 || month > 12 || day < 1 || day > days_in_month[month])
+		if (month >= 2 && day >= 60)
+		{
+			day++;
+		}
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 366 - day);
+	} else
 	{
-		printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
-		return;
+		if (month == 2 && day == 60)
+		{
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+		}
+		else
+		{
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 365 - day);
+		}
 	}
-
-	remaining_days = total_days - (day - 1);
-	for (int i = 1; i < month; i++)
-	{
-		remaining_days -= days_in_month[i];
-	}
-
-	printf("Day of the year: %d\n", total_days - remaining_days);
-	printf("Remaining days: %d\n", remaining_days);
 }
