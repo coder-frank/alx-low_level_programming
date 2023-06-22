3453100feddae8060e793938d8f59fd186a586e4
Ebuka Mbanusi <90049318+coder-frank@users.noreply.github.com>
2023-06-22 08:08:04 +0100

    Create 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include <ctype.h>
+#include "main"
+/**
+ * print_most_numbers - prints all numbers except for 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		if (i == 2 || i == 4)
+		{
+			continue;
+		}
+		_putchar(i);
+		_putchar('\n');
+}
