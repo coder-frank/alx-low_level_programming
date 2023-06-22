77cb6b50a05da086f24f9031b93c65a6e40d0ccb
Ebuka Mbanusi <90049318+coder-frank@users.noreply.github.com>
2023-06-22 14:12:22 +0100

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -10,9 +10,6 @@ int mul(int num1, int num2)
 	int result;
 
 	result = num1 * num2;
-	if (num1 < 0 || num2 < 0)
-	{
-		result = result * -1;
-	}
-	return (result);
+	result *= -1;
+	return (result * -1);
 }
