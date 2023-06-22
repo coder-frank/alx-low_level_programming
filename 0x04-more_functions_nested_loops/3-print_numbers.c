8e9aed826b4bb65eac032e70cf08052ee70dfb35
Ebuka Mbanusi <90049318+coder-frank@users.noreply.github.com>
2023-06-22 07:55:54 +0100

    Create 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include <ctype.h>
+#include "main"
+/**
+ * _isupper - Checks if a character is a number
+ * @num: The character to be checked
+ * Return: returns 1 if the character is a digit, otherwise 0
+ */
+void print_numbers(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
