d992e2d53fd794b9602da1468bd5380c0cee920d
Ebuka Mbanusi <90049318+coder-frank@users.noreply.github.com>
2023-06-22 13:51:30 +0100

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,4 +1,4 @@
-#include "main"
+#include "main.h"
 /**
  * _isupper - Checks if a character is a number
  * @num1: The first number to be multiplied with
@@ -8,7 +8,11 @@
 int mul(int num1, int num2)
 {
 	int result;
-	
+
 	result = num1 * num2;
+	if (num1 < 0 || num2 < 0)
+	{
+		result = result * -1;
+	}
 	return (result);
 }
