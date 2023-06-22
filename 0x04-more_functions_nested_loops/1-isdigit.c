32c276e3183d96d17728a0f32fa79e70066e077d
Ebuka Mbanusi <90049318+coder-frank@users.noreply.github.com>
2023-06-22 07:40:21 +0100

    Create 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include <ctype.h>
+#include "main"
+/**
+ * _isupper - Checks if a character is a number
+ * @num: The character to be checked
+ * Return: returns 1 if the character is a digit, otherwise 0
+ */
+int _isdigit(int num)
+{
+	int result;
+	
+	if(isdigit(num))
+	{
+		result = 1;
+	} else
+	{
+		result = 0;
+	}
+	return (result);
+}
