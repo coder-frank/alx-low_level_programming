0bd5b99bd55d5503d03a98d6c6d50d618ad7b0e8
Ebuka Mbanusi <90049318+coder-frank@users.noreply.github.com>
2023-06-22 16:19:35 +0100

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,9 +7,9 @@ void fizz_buzz(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	for (i = 1; i < 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if ((i % 3) == 0 && (i % 5) == 0)
 		{
 			printf("Fizz Buzz ");
 		}
@@ -24,6 +24,7 @@ void fizz_buzz(void)
 			printf("%d ", i);
 		}
 	}
+	printf("Buzz");
 	putchar('\n');
 }
 /**
