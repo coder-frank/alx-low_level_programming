c8c0b121faaaeb8e35a2d87a97d7bac8f4daebb5
Ebuka Mbanusi <90049318+coder-frank@users.noreply.github.com>
2023-06-22 16:01:17 +0100

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,16 +6,11 @@
 void print_line(int n)
 {
 	int i;
-	
-	if (n <= 0)
-	{
-		_putchar('\n');
-		return;
-	}
 
 	for (i = 0; i <= n; i++)
 	{
-		_putchar('_');
+		if (n > 0)
+			_putchar('_');
 	}
 	_putchar('\n');
 }
