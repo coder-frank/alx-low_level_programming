8bd998a96d9034f171b185eaed92d9992eab82c7
Ebuka Mbanusi <90049318+coder-frank@users.noreply.github.com>
2023-06-22 08:15:29 +0100

    Create 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include <ctype.h>
+#include "main"
+/**
+ * more_numbers - prints all numbers from 0 - 14
+ * 10 times
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
