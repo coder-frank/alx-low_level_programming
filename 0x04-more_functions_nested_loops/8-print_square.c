65be87f07cc8e5047dba7021272848cc3c0ef083
Ebuka Mbanusi <90049318+coder-frank@users.noreply.github.com>
2023-06-22 16:31:22 +0100

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
  * print_square - prints a square shape using # to the terminal for a number of times
- * @si: The number of time the shape /$ should be displayed
+ * @size: The number of time the shape # should be displayed
  */
 void print_square(int size)
 {
 	int i;
 	int j;
-	
+
 	if (size <= 0)
 	{
 		_putchar('\n');
