dce08382b155e2e66590d16d89eb9056c12e14c9
coder-frank <frankemmanuel249@gmail.com>
2023-06-22 04:23:13 -0700

    g

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * fizz_buzz: prints fizz, buzz or fizz buzz for multiples of 3, 5 or both respectively
+ * fizz_buzz - prints fizz, buzz or fizz buzz for multiples of 3, 5 or both respectively
  */
 void fizz_buzz(void)
 {
 	int i;
-	
+
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
@@ -30,8 +30,8 @@ void fizz_buzz(void)
  * Return: Always 0 (Success)
  */
 
-int main()
+int main(void)
 {
 	fizz_buzz();
-	return 0;
+	return (0);
 }
