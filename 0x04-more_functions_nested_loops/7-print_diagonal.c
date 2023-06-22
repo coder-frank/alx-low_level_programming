d88329d4dade61e9cb86f0ce613922e03c159b72
Ebuka Mbanusi <90049318+coder-frank@users.noreply.github.com>
2023-06-22 16:51:29 +0100

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,4 +1,4 @@
-#include "main"
+#include "main.h"
 /**
  * print_diagonal - prints a diagonal shape of /$ to the terminal for a number of times
  * @n: The number of time the shape /$ should be displayed
@@ -7,21 +7,19 @@ void print_diagonal(int n)
 {
 	int i;
 	int j;
-	
-	if (n == 0)
-	{
-		_putchar('\n');
-		return (0);
-	}
 
-	for (i = 0; i <= n; i++)
+	if (n > 0)
 	{
-		for (int j = 0; j < i; j++)
+		for (i = 0; i <= n; i++)
 		{
-			_putchar(' ');
+			for (int j = 0; j < i; j++)
+				_putchar(' ');
+			_putchar('\');
+			_putchar('\n');
 		}
-		_putchar('\$');
+	} else
+	{
 		_putchar('\n');
 	}
-	_putchar('\n');
+	
 }
