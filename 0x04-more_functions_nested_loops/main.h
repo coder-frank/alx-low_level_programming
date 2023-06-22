dcd956cbf23249383766219bde8b687ed402810e
Ebuka Mbanusi <90049318+coder-frank@users.noreply.github.com>
2023-06-22 08:09:06 +0100

    Update main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,6 +7,6 @@ int _isupper(char letter);
 int _isdigit(int num);
 int mul(int num1, int num2);
 void print_numbers(void);
-void print_most_numbers(void)
+void print_most_numbers(void);
 
 #endif /* MAIN_H */
