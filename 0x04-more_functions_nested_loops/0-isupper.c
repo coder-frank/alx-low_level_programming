252662c1bfa97533eec74ebc7671da3a95884ddc
Ebuka Mbanusi <90049318+coder-frank@users.noreply.github.com>
2023-06-22 07:26:40 +0100

    Create 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main"
+#include <ctype.h>
+/**
+ * _isupper - Checks if a character is an upper case
+ * @letter: The character to be checked
+ * Return: returns 1 if the character is an uppercase, otherwise 0
+ */
+int _isupper(char letter)
+{
+	int result;
+	
+	if(isupper(letter))
+	{
+		result = 1;
+	} else
+	{
+		result = 0;
+	}
+	return (result);
+}
