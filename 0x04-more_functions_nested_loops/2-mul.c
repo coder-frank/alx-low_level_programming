45761cfb665de2a2805efe2ee31e5e571d363f86
Ebuka Mbanusi <90049318+coder-frank@users.noreply.github.com>
2023-06-22 07:45:42 +0100

    Create 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main"
+/**
+ * _isupper - Checks if a character is a number
+ * @num1: The first number to be multiplied with
+ * @num2: The second number to be multiplied to
+ * Return: returns the product of the two numbers
+ */
+int mul(int num1, int num2)
+{
+	int result;
+	
+	result = num1 * num2;
+	return (result);
+}
