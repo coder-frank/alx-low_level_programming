99debde9da3753215a4f0eb0bf5093122424be8b
Ebuka Mbanusi <90049318+coder-frank@users.noreply.github.com>
2023-06-26 07:49:53 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,18 +2,19 @@
 #include <string.h>
 #include "main.h"
 /**
- * print_rev - prints out a reverse order of a variable
+ * rev_string - changes the order of a variable to a reverse order
  * @s: the variable passed
  */
 void rev_string(char *s)
 {
 	int size;
 	int i;
-	char c[10];
 	int pos;
 
 	pos = 0;
 	size = strlen(s);
+	char c[size];
+
 	for (i = size - 1; i >= 0; i--)
 	{
 		c[pos] = s[i];
