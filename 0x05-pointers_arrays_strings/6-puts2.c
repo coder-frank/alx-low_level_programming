43d298d71f6c75d04887548d29c838ce1f0d037a
Ebuka Mbanusi <90049318+coder-frank@users.noreply.github.com>
2023-06-26 08:13:08 +0100

    Create 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * puts2 - prints out the even numbers of a string
+ * @s: the string passed
+ */
+void puts2(char *s)
+{
+	int size;
+	int i;
+	char *tempC;
+	int temp;
+
+	size = strlen(s);
+	for (i = 0; i < size; i++)
+	{
+		tempC = &s[i];
+		temp = *tempC - '0';
+
+		if (temp % 2 == 0)
+			printf("%d", temp);
+	}
+	printf("\n");
+}
