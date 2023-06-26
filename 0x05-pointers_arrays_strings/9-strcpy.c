dcbd362937482fa5d18f480ecb4a1421f113c19c
Ebuka Mbanusi <90049318+coder-frank@users.noreply.github.com>
2023-06-26 08:52:46 +0100

    Create 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * print_array - prints array
+ * @a: the array passed
+ * @n: the size of the array
+ */
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+}
