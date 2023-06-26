bc7a7383c4429d900325b68375e98858cc29964e
Ebuka Mbanusi <90049318+coder-frank@users.noreply.github.com>
2023-06-26 07:00:15 +0100

    Create 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strlen - gets the length of a string
+ * @s: the string to be checked
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	return (strlen(*s);
+}
