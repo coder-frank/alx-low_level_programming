c970de957f446ab872a67f369ab5fec9f137f9fe
Ebuka Mbanusi <90049318+coder-frank@users.noreply.github.com>
2023-06-26 10:11:14 +0100

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,6 +11,7 @@ int _atoi(char *s)
 {
 	int i;
 	int result;
+	int isN = 0;
 	int size = strlen(s);
 	char *temp = (char *)malloc((size + 1) * sizeof(char));
 
@@ -20,14 +21,24 @@ int _atoi(char *s)
 	{
 		if (s[i] >= '0' && s[i] <= '9')
 		{
+			if (s[i - 1] == '-')
+				isN = 1;
 			temp[tempIndex] = s[i];
 			tempIndex++;
+
+			if (s[i + 1] == ' ')
+			{
+				if (s[i - 3] == ' ')
+					isN = 0;
+				break;
+			}
 		}
 	}
 	temp[tempIndex] = '\0';
 
 	result = atoi(temp);
 	free(temp);
-
+	if (isN == 1)
+		result *= -1;
 	return result;
 }
