ad8f8a867c2bdd6a1535caa04a063718b09220e0
Ebuka Mbanusi <90049318+coder-frank@users.noreply.github.com>
2023-06-26 08:55:02 +0100

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,9 +2,10 @@
 #include <string.h>
 #include "main.h"
 /**
- * print_array - prints array
- * @a: the array passed
- * @n: the size of the array
+ * _strcpy - copy string from one source to a destination
+ * @dest: the destination passed
+ * @src: the source passed
+ * Return: The copy of the string
  */
 char *_strcpy(char *dest, char *src)
 {
