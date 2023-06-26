77434ea3c2f69c2a223e6ef60643780bea84c955
Ebuka Mbanusi <90049318+coder-frank@users.noreply.github.com>
2023-06-26 09:38:58 +0100

    Create 100-atio.c

---
+++ 0x05-pointers_arrays_strings/100-atio.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * _atoi - convert string to number
+ * @s: the string to be passed
+ * Return: The integer conversion
+ */
+int _atoi(char *s)
+{
+	return atoi(s);
+}
