035e7bc764c2fb7ed2b32db24ae163377edae400
Ebuka Mbanusi <90049318+coder-frank@users.noreply.github.com>
2023-07-04 12:43:52 +0100

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,7 +5,6 @@
  * _strchr - locates a character in a string
  * @s: pointer to the string to search
  * @c: character to locate
- *
  * Return: Pointer to the first occurrence of the character c
  * in the string s, or NULL if the character is not found
  */
