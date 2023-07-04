aa12a089773fe5a94acd20ef6a4aaa563572cd6b
Ebuka Mbanusi <90049318+coder-frank@users.noreply.github.com>
2023-07-04 13:00:39 +0100

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 /**
  * _strspn - gets the length of a prefix substring
  * @s: pointer to the string to search
@@ -10,28 +11,5 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int length = 0;
-	int match;
-
-	while (*s)
-	{
-		match = 0;
-		while (*accept)
-		{
-			if (*s == *accept)
-			{
-				match = 1;
-				break;
-			}
-			accept++;
-		}
-
-		if (!match)
-			break;
-
-		length++;
-		s++;
-	}
-
-	return (length);
+	return (strspn(s, accept));
 }
