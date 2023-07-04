87e5e03dbf66ca18802ed9e7ad81d5c9911b54d5
Ebuka Mbanusi <90049318+coder-frank@users.noreply.github.com>
2023-07-04 12:43:03 +0100

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,7 +7,7 @@
  * @c: character to locate
  *
  * Return: Pointer to the first occurrence of the character c
- *         in the string s, or NULL if the character is not found
+ * in the string s, or NULL if the character is not found
  */
 char *_strchr(char *s, char c)
 {
