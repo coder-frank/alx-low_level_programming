5b9d65e14156bc0f1cba476b257a6063d9f71cc5
coder-frank <frankemmanuel249@gmail.com>
2023-07-05 08:05:17 +0100

    strlenmn commit

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * _strlen_recursion - prints out the length of a string
+ * @s: The string to be printed out
+*/
+int _strlen_recursion(char *s)
+{
+	return (strlen(s));
+}
