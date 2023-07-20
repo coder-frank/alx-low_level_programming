17c61a0e914e61c8306a84e86aa8523ee958fbbf
coder-frank <frankemmanuel249@gmail.com>
2023-07-20 14:39:18 +0100

    My Way

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,49 +1,59 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdarg.h>
-/**
- * print_all - prints strings
- * @format: The format
-*/
+
 void print_all(const char * const format, ...)
 {
-	va_list list;
-	unsigned int j = 0, start = 0;
-	char *p;
+	va_list ap;
+	int i, count = 0;
+	char *s;
+	char c;
+	char allowed[4];
 
-	va_start(list, format);
-	while (format && format[j] != '\0')
+	i = 0;
+	while (format[i] != '\0')
 	{
-		switch (format[j])
-		{ case 'c':
-			switch (start)
-			{ case 1: printf(", "); }
-			start = 1;
-			printf("%c", va_arg(list, int));
-			break;
-			case 'i':
-			switch (start)
-			{ case 1: printf(", "); }
-			start = 1;
-			printf("%i", va_arg(list, int));
-			break;
-		case 'f':
-			switch (start)
-			{ case 1: printf(", "); }
-			start = 1;
-			printf("%f", va_arg(list, double));
-			break;
-		case's':
-			switch (start)
-			{ case 1: printf(", "); }
-			start = 1;
-			p = va_arg(list, char*);
-			if (p)
-			{ printf("%s", p);
-			break; }
-			printf("%p", p);
-			break; }
-		j++;
+		if (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
+		{
+			allowed[count] = format[i];
+			count++;
+		}
+		i++;
 	}
-	printf("\n");
-	va_end(list);
+
+	i = 0;
+	
+	va_start(ap, format);
+	while (i < count)
+	{
+		c = allowed[i];
+		switch (c)
+        {
+        case 'c':
+            printf("%c", va_arg(ap, int));
+            break;
+        case 'i':
+            printf("%d", va_arg(ap, int));
+            break;
+        case 'f':
+            printf("%f", va_arg(ap, double));
+            break;
+        case 's':
+            s = va_arg(ap, char *);
+            if (s == NULL)
+                printf("(nil)");
+            else
+                printf("%s", s);
+            break;
+        default:
+            break;
+        }
+
+		if (i < count - 1)
+			printf(", ");
+		i++;
+	}
+	
+	putchar('\n');
+	va_end(ap);
 }
