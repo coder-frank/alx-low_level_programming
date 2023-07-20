0c99c272e170439565dd6dfa2ed2d25d58c3d9c6
coder-frank <frankemmanuel249@gmail.com>
2023-07-20 13:42:44 +0100

    fixed

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,6 +23,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (separator != NULL && i != (n - 1))
 			printf("%s", separator);
 	}
-	printf('\n');
+	putchar('\n');
 	va_end(ap);
 }
