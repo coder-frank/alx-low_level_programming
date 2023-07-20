762c209f6a3381f162cd1b4569fffcd4f9c1c989
coder-frank <frankemmanuel249@gmail.com>
2023-07-20 12:52:00 +0100

    0-fixed

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,22 +7,22 @@
 */
 int sum_them_all(const unsigned int n, ...)
 {
-    if (n == 0)
-        return 0;
-    va_list ap;
-    int i, sum;
+	if (n == 0)
+		return 0;
+	va_list ap;
+	int i, sum;
 
-    va_start(ap, n);
-    sum = 0;
-    for (i = 0; i < n; i++)
-    {
-        int num = va_arg(ap, int);
+	va_start(ap, n);
+	sum = 0;
+	for (i = 0; i < n; i++)
+	{
+		int num = va_arg(ap, int);
 
-        if (num < 0)
-            sum -= -num;
-        else
-            sum += num;
-    }
-    va_end(ap);
-    return (sum);
+		if (num < 0)
+			sum -= -num;
+		else
+			sum += num;
+	}
+	va_end(ap);
+	return (sum);
 }
